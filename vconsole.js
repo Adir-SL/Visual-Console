a5f4833484433fde13b4418ecbc4fdb0121802ea
Adir <42638312+Adir-SL@users.noreply.github.com>
2021-02-07 16:53:08 +0200

    Update vconsole.js

--- vconsole.js
+++ vconsole.js
@@ -211,11 +211,11 @@ function checkForChars(){
 function addConsole(){
     window.chevSvgIcon = '<svg style="width:24px;height:24px;pointer-events:none;" viewBox="0 0 24 24"><path fill="currentColor" d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>';
     window.pickSvgIcon = '<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M10.76,8.69A0.76,0.76 0 0,0 10,9.45V20.9C10,21.32 10.34,21.66 10.76,21.66C10.95,21.66 11.11,21.6 11.24,21.5L13.15,19.95L14.81,23.57C14.94,23.84 15.21,24 15.5,24C15.61,24 15.72,24 15.83,23.92L18.59,22.64C18.97,22.46 19.15,22 18.95,21.63L17.28,18L19.69,17.55C19.85,17.5 20,17.43 20.12,17.29C20.39,16.97 20.35,16.5 20,16.21L11.26,8.86L11.25,8.87C11.12,8.76 10.95,8.69 10.76,8.69M15,10V8H20V10H15M13.83,4.76L16.66,1.93L18.07,3.34L15.24,6.17L13.83,4.76M10,0H12V5H10V0M3.93,14.66L6.76,11.83L8.17,13.24L5.34,16.07L3.93,14.66M3.93,3.34L5.34,1.93L8.17,4.76L6.76,6.17L3.93,3.34M7,10H2V8H7V10" /></svg>';
-    document.body.innerHTML += "<div id='velements_"+window.checkTime+"' class='vConUndetect'><div style='display:flex;position:sticky;top:0;background-color:#ffffff;border-radius:0;'><a class='tabClass' onclick='vconConsoleFunc();'>Console</a><a class='tabClass tabSelect' onclick='vconElemFunc();'>Elements</a></div><div>Coming Soon...</div></div><div id='vconsole_"+window.checkTime+"' class='vConUndetect'><div style='display:flex;position:sticky;top:0;background-color:#ffffff;border-radius:0;'><a class='tabClass tabSelect' onclick='vconConsoleFunc();'>Console</a><a class='tabClass' onclick='vconElemFunc();'>Elements</a></div><details class='vConUndetect' open><summary class='vConUndetect'></summary></details></div><div class='flexDiv vConUndetect' id='flex_"+window.checkTime+"'><input class='vConUndetect' spellcheck='false' autocapitalize='none' list='historyList' placeholder='Type code here...'></input><datalist id='historyList' class='vConUndetect'></datalist><button id=ibutton_"+window.checkTime+" class='vConUndetect' onclick='iFunc(event);' title='Run code (Enter)'>"+window.chevSvgIcon+"</button><button id=pbutton_"+window.checkTime+" class='vConUndetect' onclick='selectFromPage();' title='Select an element from the page (Ctrl+H)'>"+window.pickSvgIcon+"</button></div><button id='vbutton_"+window.checkTime+"' class='vConUndetect' onclick='toggleConsole();' title='Toggle this panel (~)'>"+window.chevSvgIcon+"</button>";
+    document.body.innerHTML += "<div id='velements_"+window.checkTime+"' class='vConUndetect'><div class='vConUndetect' style='display:flex;position:sticky;top:0;background-color:#ffffff;border-radius:0;'><a class='tabClass vConUndetect' onclick='vconConsoleFunc();'>Console</a><a class='tabClass vConUndetect tabSelect' onclick='vconElemFunc();'>Elements</a></div><div>Coming Soon...</div></div><div id='vconsole_"+window.checkTime+"' class='vConUndetect'><div style='display:flex;position:sticky;top:0;background-color:#ffffff;border-radius:0;'><a class='tabClass vConUndetect tabSelect' onclick='vconConsoleFunc();'>Console</a><a class='tabClass vConUndetect' onclick='vconElemFunc();'>Elements</a></div><details class='vConUndetect' open><summary class='vConUndetect'></summary></details></div><div class='flexDiv vConUndetect' id='flex_"+window.checkTime+"'><input class='vConUndetect' spellcheck='false' autocapitalize='none' list='historyList' placeholder='Type code here...'></input><datalist id='historyList' class='vConUndetect'></datalist><button id=ibutton_"+window.checkTime+" class='vConUndetect' onclick='iFunc(event);' title='Run code (Enter)'>"+window.chevSvgIcon+"</button><button id=pbutton_"+window.checkTime+" class='vConUndetect' onclick='selectFromPage();' title='Select an element from the page (Ctrl+H)'>"+window.pickSvgIcon+"</button></div><button id='vbutton_"+window.checkTime+"' class='vConUndetect' onclick='toggleConsole();' title='Toggle this panel (~)'>"+window.chevSvgIcon+"</button>";
 
     //Console CSS
     const ConsoleStyles = s =>(d=>{d.head.appendChild(d.createElement("style")).innerHTML=s})(document);
-    ConsoleStyles("#vconsole_"+window.checkTime+"{ position:fixed; font-weight:normal; right:16px; top:16px; width:300px; max-width:50%; height:150px; padding:8px; padding-top:0; overflow:auto; scroll-behavior:smooth; background-color:#ffffff; box-shadow:0 4px 16px 0 rgba(0,0,0,.2); font-family:monospace; border-radius: 8px;transform-origin:center;transition:transform 100ms ease-in-out, opacity 100ms ease-in-out; }"+"#velements_"+window.checkTime+" div{ margin-bottom:4px; padding:8px;}"+"#vconsole_"+window.checkTime+" div{ margin-bottom:4px; padding:8px; border-radius:4px; color:#000000; background-color:#ededed; }"+"#vconsole_"+window.checkTime+" summary:empty ~ div:last-of-type{ animation:slideOver 200ms ease-out; }"+window.checkTime+" summary:empty{ height:0; }"+"#vbutton_"+window.checkTime+"{ position:fixed; right:32px; top:32px; padding:1px; margin:0; opacity:1; width:32px; height:32px; border:none; transform:translate(80%,-80%); outline:none; background-color:skyblue; color:#ffffff; font-weight:bold; border-radius:50%; display: flex; align-items: center; justify-content: center; cursor:pointer; box-shadow:0 4px 8px 0 rgba(0,0,0,.2); } @keyframes slideOver{ 0%{transform: translateX(-100%);} 100%{transform: translateX(0px);}}"+"#vconsole_"+window.checkTime+" + .flexDiv{ display:flex; align-items:stretch; position:fixed; top:180px; right:16px; width: 300px; padding: 8px; margin-top: 8px; border-radius:8px; box-shadow:0 4px 16px 0 rgba(0,0,0,.2); background-color:#ffffff;transition:opacity 100ms ease-in-out; }"+"#vconsole_"+window.checkTime+" + .flexDiv input{ width:calc(100% - 64px); background-color:#ffffff; border-radius:4px; padding: 8px; border:1px solid #cccccc; outline:none;margin-right:4px; font-size: 16px; -webkit-appearance: none; -moz-appearance: none; appearance: none; }"+"#ibutton_"+window.checkTime+"{ border-radius:4px; border:none; box-shadow:0 0 0 2px skyblue inset; outline:none; background-color:#ffffff; color:#ffffff; padding:1px; margin:0; opacity:1; width:36px; font-weight:bold; display: flex; align-items: center; justify-content: center; cursor:pointer; }"+"#pbutton_"+window.checkTime+"{ border-radius:4px; border:none; outline:none; background-color:#ffffff; color:grey; padding:1px; margin:0; opacity:1; width:36px; font-weight:bold; display: flex; align-items: center; justify-content: center; cursor:pointer;margin-right: 4px; }"+"#vbutton_"+window.checkTime+" svg{ transition:transform 200ms ease-in-out; }"+"#ibutton_"+window.checkTime+" svg{ transform:rotate(-90deg); }"+"#ibutton_"+window.checkTime+" path{ fill:skyblue; }"+"#ibutton_"+window.checkTime+":active, #ibutton_"+window.checkTime+":focus, #vbutton_"+window.checkTime+":active, #vbutton_"+window.checkTime+":focus{ box-shadow: 0 0 0 2px cornflowerblue inset, 0 4px 8px 0 rgba(0,0,0,.2); }"+"#ibutton_"+window.checkTime+":hover, #vbutton_"+window.checkTime+":hover{ filter:brightness(104%); }"+"#pbutton_"+window.checkTime+":hover{ filter:brightness(115%); }"+"#ibutton_"+window.checkTime+":focus path, #vbutton_"+window.checkTime+":focus path{ fill:cornflowerblue; }"+"#pbutton_"+window.checkTime+":focus{ color:#ffffff; background-color:grey; }"+"#vbutton_"+window.checkTime+" svg, #ibutton_"+window.checkTime+" svg, #pbutton_"+window.checkTime+" svg{ pointer-events:none; }")
+    ConsoleStyles("#vconsole_"+window.checkTime+"{ position:fixed; font-weight:normal; right:16px; top:16px; width:300px; max-width:50%; height:150px; padding:8px; padding-top:0; overflow:auto; scroll-behavior:smooth; background-color:#ffffff; box-shadow:0 4px 16px 0 rgba(0,0,0,.2); font-family:monospace; border-radius: 8px;transform-origin:center;transition:transform 100ms ease-in-out, opacity 100ms ease-in-out; }"+"#velements_"+window.checkTime+" div{ margin-bottom:4px; padding:8px;}"+"#vconsole_"+window.checkTime+" div{ margin-bottom:4px; padding:8px; border-radius:4px; color:#000000; background-color:#ededed; }"+"#vconsole_"+window.checkTime+" summary:empty ~ div:last-of-type{ animation:slideOver 200ms ease-out; }"+window.checkTime+" summary:empty{ height:0; }"+"#vbutton_"+window.checkTime+"{ position:fixed; right:32px; top:32px; padding:1px; margin:0; opacity:1; width:32px; height:32px; border:none; transform:translate(80%,-80%); outline:none; background-color:skyblue; color:#ffffff; font-weight:bold; border-radius:50%; display: flex; align-items: center; justify-content: center; cursor:pointer; box-shadow:0 4px 8px 0 rgba(0,0,0,.2); } @keyframes slideOver{ 0%{transform: translateX(-100%);} 100%{transform: translateX(0px) sacleY(1);}}"+"#vconsole_"+window.checkTime+" + .flexDiv{ display:flex; align-items:stretch; position:fixed; top:180px; right:16px; width: 300px; padding: 8px; margin-top: 8px; border-radius:8px; box-shadow:0 4px 16px 0 rgba(0,0,0,.2); background-color:#ffffff;transition:opacity 100ms ease-in-out; }"+"#vconsole_"+window.checkTime+" + .flexDiv input{ width:calc(100% - 64px); background-color:#ffffff; border-radius:4px; padding: 8px; border:1px solid #cccccc; outline:none;margin-right:4px; font-size: 16px; -webkit-appearance: none; -moz-appearance: none; appearance: none; }"+"#ibutton_"+window.checkTime+"{ border-radius:4px; border:none; box-shadow:0 0 0 2px skyblue inset; outline:none; background-color:#ffffff; color:#ffffff; padding:1px; margin:0; opacity:1; width:36px; font-weight:bold; display: flex; align-items: center; justify-content: center; cursor:pointer; }"+"#pbutton_"+window.checkTime+"{ border-radius:4px; border:none; outline:none; background-color:#ffffff; color:grey; padding:1px; margin:0; opacity:1; width:36px; font-weight:bold; display: flex; align-items: center; justify-content: center; cursor:pointer;margin-right: 4px; }"+"#vbutton_"+window.checkTime+" svg{ transition:transform 200ms ease-in-out; }"+"#ibutton_"+window.checkTime+" svg{ transform:rotate(-90deg); }"+"#ibutton_"+window.checkTime+" path{ fill:skyblue; }"+"#ibutton_"+window.checkTime+":active, #ibutton_"+window.checkTime+":focus, #vbutton_"+window.checkTime+":active, #vbutton_"+window.checkTime+":focus{ box-shadow: 0 0 0 2px cornflowerblue inset, 0 4px 8px 0 rgba(0,0,0,.2); }"+"#ibutton_"+window.checkTime+":hover, #vbutton_"+window.checkTime+":hover{ filter:brightness(104%); }"+"#pbutton_"+window.checkTime+":hover{ filter:brightness(115%); }"+"#ibutton_"+window.checkTime+":focus path, #vbutton_"+window.checkTime+":focus path{ fill:cornflowerblue; }"+"#pbutton_"+window.checkTime+":focus{ color:#ffffff; background-color:grey; }"+"#vbutton_"+window.checkTime+" svg, #ibutton_"+window.checkTime+" svg, #pbutton_"+window.checkTime+" svg{ pointer-events:none; }")
 
     //Console CSS - Mobile
     const mobileStyles = s =>(d=>{d.head.appendChild(d.createElement("style")).innerHTML=s})(document);
@@ -223,7 +223,7 @@ function addConsole(){
 
     //Elements CSS
     const ElementsStyles = s =>(d=>{d.head.appendChild(d.createElement("style")).innerHTML=s})(document);
-    ElementsStyles("#velements_"+window.checkTime+"{ position:fixed; font-weight:normal; right:16px; top:16px; width:300px; max-width:50%; height:150px; padding:8px; padding-top:0; overflow:auto; scroll-behavior:smooth; background-color:#ffffff; box-shadow:0 4px 16px 0 rgba(0,0,0,.2); font-family:monospace; border-radius: 8px;transform-origin:center;transform:translateX(-100%); opacity:0; pointer-events:none; transition:transform 100ms ease-in-out, opacity 100ms ease-in-out; }")
+    ElementsStyles("#velements_"+window.checkTime+"{ position:fixed; font-weight:normal; right:16px; top:16px; width:300px; max-width:50%; height:150px; padding:8px; padding-top:0; overflow:auto; scroll-behavior:smooth; background-color:#ffffff; box-shadow:0 4px 16px 0 rgba(0,0,0,.2); font-family:monospace; border-radius: 8px;transform-origin:center;transform:translateX(100%); opacity:0; pointer-events:none; transition:transform 100ms ease-in-out, opacity 100ms ease-in-out; }")
 
     //Links CSS
     const LinkStyles = s =>(d=>{d.head.appendChild(d.createElement("style")).innerHTML=s})(document);
@@ -275,18 +275,31 @@ function addConsole(){
 }
 function toggleConsole(){
     if(window.hideConsole != true){
-        document.getElementById('vconsole_'+window.checkTime).style.transform = "scaleY(0)";
-        document.getElementById('vconsole_'+window.checkTime).style.opacity = 0;
-        document.getElementById('vconsole_'+window.checkTime).style.pointerEvents = "none";
-        document.getElementById('vconsole_'+window.checkTime).style.overflow = "hidden";
+        if(document.getElementsByClassName('tabSelect')[1].innerText == "Elements"){
+            document.getElementById('velements_'+window.checkTime).style.transform = "translateX(0) scaleY(0)";
+            document.getElementById('velements_'+window.checkTime).style.opacity = 0;
+            document.getElementById('velements_'+window.checkTime).style.pointerEvents = "none";
+            document.getElementById('velements_'+window.checkTime).style.overflow = "hidden";
+        }else{
+            document.getElementById('vconsole_'+window.checkTime).style.transform = " translateX(0)scaleY(0)";
+            document.getElementById('vconsole_'+window.checkTime).style.opacity = 0;
+            document.getElementById('vconsole_'+window.checkTime).style.pointerEvents = "none";
+            document.getElementById('vconsole_'+window.checkTime).style.overflow = "hidden";
+        }
         document.getElementById('flex_'+window.checkTime).style.opacity = 0;
         document.getElementById('flex_'+window.checkTime).style.pointerEvents = "none";
         document.getElementById('vbutton_'+window.checkTime).getElementsByTagName("svg")[0].style.transform = "rotate(-90deg)";
         window.hideConsole = true;
     }else{
-        document.getElementById('vconsole_'+window.checkTime).style.transform = "scaleY(1)";
-        document.getElementById('vconsole_'+window.checkTime).style.opacity = 1;
-        document.getElementById('vconsole_'+window.checkTime).style.pointerEvents = "all";
+        if(document.getElementsByClassName('tabSelect')[1].innerText == "Elements"){
+            document.getElementById('velements_'+window.checkTime).style.transform = "translateX(0) scaleY(1)";
+            document.getElementById('velements_'+window.checkTime).style.opacity = 1;
+            document.getElementById('velements_'+window.checkTime).style.pointerEvents = "all";
+        }else{
+            document.getElementById('vconsole_'+window.checkTime).style.transform = "translateX(0) scaleY(1)";
+            document.getElementById('vconsole_'+window.checkTime).style.opacity = 1;
+            document.getElementById('vconsole_'+window.checkTime).style.pointerEvents = "all";
+        }
         document.getElementById('flex_'+window.checkTime).style.opacity = 1;
         document.getElementById('flex_'+window.checkTime).style.pointerEvents = "all";
         document.getElementById('vbutton_'+window.checkTime).getElementsByTagName("svg")[0].style.transform = "rotate(0deg)";
@@ -380,20 +393,28 @@ function selectFromPage(){
     }
 }
 function vconElemFunc(){
-    document.getElementById("velements_"+window.checkTime).style.transform = "translateX(0)";
+    document.getElementById("velements_"+window.checkTime).style.transform = "translateX(0) scaleY(1)";
     document.getElementById("velements_"+window.checkTime).style.opacity = 1;
     document.getElementById("velements_"+window.checkTime).style.pointerEvents = "all";
-    document.getElementById("vconsole_"+window.checkTime).style.transform = "translateX(100%)";
+    document.getElementById("vconsole_"+window.checkTime).style.transform = "translateX(-100%) scaleY(1)";
     document.getElementById("vconsole_"+window.checkTime).style.opacity = 0;
     document.getElementById("vconsole_"+window.checkTime).style.pointerEvents = "none";
+    document.getElementById("velements_"+window.checkTime).getElementsByClassName("tabClass")[0].className = "tabClass";
+    document.getElementById("velements_"+window.checkTime).getElementsByClassName("tabClass")[1].className = "tabClass tabSelect";
+    document.getElementById("vconsole_"+window.checkTime).getElementsByClassName("tabClass")[0].className = "tabClass";
+    document.getElementById("vconsole_"+window.checkTime).getElementsByClassName("tabClass")[1].className = "tabClass tabSelect";
 }
 function vconConsoleFunc(){
-    document.getElementById("velements_"+window.checkTime).style.transform = "translateX(-100%)";
+    document.getElementById("velements_"+window.checkTime).style.transform = "translateX(100%) scaleY(1)";
     document.getElementById("velements_"+window.checkTime).style.opacity = 0;
     document.getElementById("velements_"+window.checkTime).style.pointerEvents = "none";
-    document.getElementById("vconsole_"+window.checkTime).style.transform = "translateX(0)";
+    document.getElementById("vconsole_"+window.checkTime).style.transform = "translateX(0) scaleY(1)";
     document.getElementById("vconsole_"+window.checkTime).style.opacity = 1;
     document.getElementById("vconsole_"+window.checkTime).style.pointerEvents = "all";
+    document.getElementById("velements_"+window.checkTime).getElementsByClassName("tabClass")[1].className = "tabClass";
+    document.getElementById("velements_"+window.checkTime).getElementsByClassName("tabClass")[0].className = "tabClass tabSelect";
+    document.getElementById("vconsole_"+window.checkTime).getElementsByClassName("tabClass")[1].className = "tabClass";
+    document.getElementById("vconsole_"+window.checkTime).getElementsByClassName("tabClass")[0].className = "tabClass tabSelect";
 }
 
 window.checkTime = new Date().valueOf();
